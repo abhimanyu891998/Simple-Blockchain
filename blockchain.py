d131e4b49905a46ee50e731c50c79dc47da832c8
Abhimanyue Singh Tanwar <singhabhimanyu186@gmail.com>
2020-05-10 21:48:09 +0530

    Block header defined

--- blockchain.py
+++ blockchain.py
@@ -1,31 +1,56 @@
+import hashlib
+from time import time
+
 class Blockchain(object):
+
     def __init__(self):
         self.chain = []
         self.transaction_list = []
 
-    def generate_block(self):
-        #Creates a new block 
-        pass
-
     def new_transaction(self, sender, recipient, amount):
         #Adds a transaction to the transaction list
-        pass
+        return 0
 
     def generate_hash(self):
         #Generates a hash
-        pass
+        return 0
+
+    def previous_hash(self):
+        #returns the previous hash
+        return 0
 
     def generate_merkle_tree(self):
         #Creates a merkle tree and returns the root
-        pass
+        return 0
 
     def generate_proof(self):
         #Returns a proof of the transaction
-        pass
+        return 0
 
     def verify_transaction(self):
         #Verify if a transaction T is in the blockchain
-        pass
+        return 0
+
+    def generate_block(self):
+        #Creates a new block
+        block = {
+            'previous_hash': None,
+            'timestamp': time(),
+            'nonce': len(self.chain) + 1, #Confused here, they have not given any details for nonce
+            'merkle_tree_root': None,
+            'hash': None
+        }
+
+        block.previous_hash = self.previous_hash()
+        block.merkle_tree_root = self.generate_merkle_tree()
+        block.hash = self.generate_hash()
+
+        #Resetting the current list of transactions
+        self.transaction_list = []
+        #Adding a new block
+        self.chain.append(block)
+
+        return block
 
 
 def main():
